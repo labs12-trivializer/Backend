5810452d3ff95d8336272be8d6db78f79569dcf6
Kory <korynewtondev@gmail.com>
2019-04-25 14:14:48 -0700

    tiers table

--- data/migrations/20190424165604_create_tables.js
+++ data/migrations/20190424165604_create_tables.js
@@ -1,8 +1,23 @@
-
-exports.up = function(knex, Promise) {
-  
+exports.up = function(knex) {
+  return (
+    knex.schema
+      //Tiers Table
+      .createTable('tiers', tbl => {
+        tbl.increments();
+        tbl
+          .string('name', 128)
+          .notNullable()
+          .unique();
+        tbl.timestamp('timestamps').defaultTo(knex.fn.now());
+        tbl.integer('game_limit').notNullable();
+        tbl.integer('round_limit').notNullable();
+        tbl.integer('question_limit').notNullable();
+        tbl.string('stripe_product_id').notNullable();
+        tbl.string('stripe_plan_id').notNullable();
+      })
+  );
 };
 
-exports.down = function(knex, Promise) {
-  
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists('tiers');
 };
