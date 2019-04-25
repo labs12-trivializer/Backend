e718b80a1fcdfd2f14ca9c275482e0426ef017b6
Kory <korynewtondev@gmail.com>
2019-04-25 15:22:19 -0700

    updated tiers route, added GetById()

--- routes/tiers.js
+++ routes/tiers.js
@@ -1,10 +1,28 @@
 const router = require('express').Router();
-const db = require('../data/db');
+
+const Tiers = require('../models/tiers');
 
 router.get('/', async (req, res) => {
   try {
-    const tiers = await db('tiers');
-    res.status(200).json(tiers);
+    const tiers = await Tiers.get();
+    if (tiers.length > 0) {
+      res.status(200).json(tiers);
+    } else {
+      res.status(400).json({ message: 'No tiers exist' });
+    }
+  } catch (error) {
+    res.status(500).json({ error });
+  }
+});
+router.get('/:id', async (req, res) => {
+  const { id } = req.params;
+  try {
+    const tier = await Tiers.getById(id);
+    if (tier.length > 0) {
+      res.status(200).json(tier);
+    } else {
+      res.status(400).json({ message: 'Tier does not exist' });
+    }
   } catch (error) {
     res.status(500).json({ error });
   }
