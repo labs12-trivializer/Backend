41455362300c3ce6f067f9d4ad183ad1e490476d
heyjuststart <41658450+heyjuststart@users.noreply.github.com>
2019-04-25 16:27:28 -0400

    implement auth0 getting started for APIs

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,25 @@
 const { logger, errorLogger } = require('../middleware/winston');
+const jwt = require('express-jwt');
+const jwks = require('jwks-rsa');
+
+const jwtCheck = jwt({
+      secret: jwks.expressJwtSecret({
+          cache: true,
+          rateLimit: true,
+          jwksRequestsPerMinute: 5,
+          jwksUri: 'https://dev-d9y68pfa.auth0.com/.well-known/jwks.json'
+    }),
+    audience: 'https://lambda-trivializer.herokuapp.com/',
+    issuer: 'https://dev-d9y68pfa.auth0.com/',
+    algorithms: ['RS256']
+});
 
 module.exports = server => {
   server.use(logger);
 
+  server.get('/authorized', jwtCheck, function (req, res) {
+      res.send('Secured Resource');
+  });
   // server.use('/api/auth', auth);
   // server.use('/api/users', users);
   // server.use('/api/games', games);
