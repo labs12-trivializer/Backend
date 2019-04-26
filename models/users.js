5a066920558875879109d71e5fa1d809e639392e
heyjuststart <41658450+heyjuststart@users.noreply.github.com>
2019-04-26 15:04:34 -0400

    prevent users creating other users

--- models/users.js
+++ models/users.js
@@ -19,24 +19,25 @@ async function get() {
 }
 
 async function getById(id) {
-  const user = await db('users').where({ id });
+  const user = await db('users')
+    .where({ id })
+    .first();
   return user;
 }
 
 async function insert(user) {
   return await db('users')
     .insert(user, 'id')
-    .then(ids =>
-      db('users')
-        .getById(ids[0])
-        .first()
-    );
+    .then(ids => getById(ids[0]));
 }
 
 function validate(user) {
   // joi schema
   const schema = Joi.object().keys({
-    email: Joi.string().email().max(255).required(),
+    email: Joi.string()
+      .email()
+      .max(255)
+      .required(),
     tier_id: Joi.number().integer(),
     logo_url: Joi.string().uri(),
     auth0_id: Joi.string().required(),
--- routes/users.js
+++ routes/users.js
@@ -21,18 +21,35 @@ router.get('/:id', async (req, res) => {
 router.post('/', async (req, res) => {
   const { body: user } = req;
 
+  // this gets attached via the jwtCheck middleware from line 18 (used on all user routes)
+  const currentUserId = req.user.sub;
+
   // validate input with Joi schema in from users model file.
   const validationResult = Users.validate(req.body);
-  if(validationResult.error) {
+  if (validationResult.error) {
     return res.status(422).json(validationResult);
   }
 
+  // make sure the currentUserId matches the auth0_id being posted
+  // should prevent users from creating other users
+  if (currentUserId !== user.auth0_id) {
+    return res.status(422).json({
+      error: {
+        name: 'ValidationError',
+        details: [{ message: "auth0_id does not match token's auth0 id" }]
+      }
+    });
+  }
 
-  const foundUser = await Users.find().where({ email: user.email }).first();
-  if(foundUser) {
+  // if the user existed, return that
+  const foundUser = await Users.find()
+    .where({ auth0_id: user.auth0_id })
+    .first();
+  if (foundUser) {
     return res.status(200).json(foundUser);
   }
 
+  // otherwise make a new one and return it
   const newUser = await Users.insert(user);
   return res.status(201).json(newUser);
 });
