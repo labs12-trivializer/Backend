6963f842fb1486e7a062a07c5ee8f8dd2dd62209
macjabeth <macbethjonathan@gmail.com>
2019-04-30 13:06:55 -0400

    remove restricted mw from individual answer endpoints

--- routes/answers.js
+++ routes/answers.js
@@ -1,20 +1,19 @@
 const router = require('express').Router();
 const Questions = require('../models/questions');
 const Answers = require('../models/answers');
-const restricted = require('../middleware/restricted');
 const validate = require('../middleware/validate');
 
 // C - Create
 router.post(
   '/',
-  restricted,
   validate(Answers.schema, true),
   async ({ body: newAnswer }, res) => {
     // Check that question exists
     const [question] = await Questions.get(newAnswer.question_id);
-    if (!question) return res.status(404).json({
-      message: 'That question does not exist.'
-    });
+    if (!question)
+      return res.status(404).json({
+        message: 'That question does not exist.'
+      });
 
     // Add the new answer
     const [answerID] = await Answers.add(newAnswer);
@@ -26,30 +25,33 @@ router.post(
 );
 
 // R - Retrieve (by question id)
-router.get('/:id', restricted, async ({ params: { id } }, res) => {
+router.get('/:id', async ({ params: { id } }, res) => {
   const answers = await Answers.filter({ question_id: id });
   answers.length
     ? res.status(200).json(answers)
-    : res.status(404).json({ message: 'There are no answers for that question.' });
+    : res
+        .status(404)
+        .json({ message: 'There are no answers for that question.' });
 });
 
 // U - Update
 router.put(
   '/:id',
-  restricted,
   validate(Answers.schema),
   async ({ params: { id }, body: changes }, res) => {
     // Check that question exists
     const [question] = await Questions.get(changes.question_id);
-    if (!question) return res.status(404).json({
-      message: 'That question does not exist.'
-    });
+    if (!question)
+      return res.status(404).json({
+        message: 'That question does not exist.'
+      });
 
     // Update the new answer and check status
     const updated = await Answers.update(id, changes);
-    if (!updated) return res.status(404).json({
-      message: 'The answer does not exist.'
-    });
+    if (!updated)
+      return res.status(404).json({
+        message: 'The answer does not exist.'
+      });
 
     // Return the updated answer
     const [answer] = await Answers.get(id);
@@ -58,13 +60,9 @@ router.put(
 );
 
 // D - Delete
-router.delete(
-  '/:id',
-  restricted,
-  async ({ params: { id } }, res) => {
-    const deleted = await Answers.remove(id);
-    deleted
-      ? res.status(200).json({ id: deleted, message: 'Answer deleted.' })
-      : res.status(404).json({ message: 'The answer does not exist.' });
-  }
-);
+router.delete('/:id', async ({ params: { id } }, res) => {
+  const deleted = await Answers.remove(id);
+  deleted
+    ? res.status(200).json({ id: deleted, message: 'Answer deleted.' })
+    : res.status(404).json({ message: 'The answer does not exist.' });
+});
