a103253643c724e11789683effc202511144a64a
heyjuststart <41658450+heyjuststart@users.noreply.github.com>
2019-04-30 11:44:12 -0400

    move token checking to routes/index
    This change is needed because it seems like all of our routes will
    require valid tokens and it'd be better to have that consolidated to one
    file at routes/index.js.

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,7 @@ const questionTypes = require('./questionTypes');
 const categories = require('./categories');
 const games = require('./games');
 const rounds = require('./rounds');
+const lookupUser = require('../middleware/lookupUser');
 
 module.exports = server => {
   server.use(logger);
@@ -14,14 +15,14 @@ module.exports = server => {
       res.send('Secured Resource');
   });
   // server.use('/api/auth', auth);
-  server.use('/api/users', users);
-  server.use('/api/games', games);
-  server.use('/api/rounds', rounds);
+  server.use('/api/users', jwtCheck, users);
+  server.use('/api/games', jwtCheck, lookupUser, games);
+  server.use('/api/rounds', jwtCheck, lookupUser, rounds);
   // server.use('/api/questions', questions);
   // server.use('/api/answers', answers);
-  server.use('/api/question_types', questionTypes);
-  server.use('/api/categories', categories);
-  server.use('/api/tiers', tiers);
+  server.use('/api/question_types', jwtCheck, questionTypes);
+  server.use('/api/categories', jwtCheck, categories);
+  server.use('/api/tiers', jwtCheck, tiers);
   server.get(/\/(?:api)?/, (req, res) => {
     res.status(200).json({ message: 'Server up & running!' });
   });
