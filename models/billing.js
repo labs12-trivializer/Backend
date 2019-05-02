6ea46fe7b962b80f620c8293895f3bf3255fa1d8
Kory <korynewtondev@gmail.com>
2019-05-02 12:34:59 -0700

    updates tier_id after successful subscription

--- models/billing.js
+++ models/billing.js
@@ -2,13 +2,26 @@ const db = require('../data/db.js');
 
 module.exports = {
   saveStripeId,
+  updateTier,
 };
 
+// save stripe_customer_id associated with user in 'user' table
 async function saveStripeId(id, stripe_customer_id) {
   try {
     await db('users')
       .where({ id })
       .update({ stripe_customer_id });
+    return;
+  } catch (error) {
+    return error;
+  }
+}
+//updates tier_id in 'users' table
+async function updateTier(id, tier) {
+  try {
+    await db('users')
+      .where({ id })
+      .update({ tier_id: tier });
     return;
   } catch (error) {
     return error;
