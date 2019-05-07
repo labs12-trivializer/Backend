5fe6bb4d95f6de569caef948cdf369e51420c137
heyjuststart <41658450+heyjuststart@users.noreply.github.com>
2019-05-06 23:44:33 -0400

    add normalized route for question_types

--- routes/questionTypes.js
+++ routes/questionTypes.js
@@ -9,4 +9,24 @@ router.get('/', async (req, res) => {
   return res.status(200).json(questionTypes);
 });
 
+router.get('/normalized', async (req, res) => {
+  const questionTypes = await QuestionTypes.find();
+
+  const normalized = {
+    result: questionTypes.map(c => c.id),
+    entities: {
+      questionTypes: questionTypes.reduce((accu, cur) => {
+        accu[cur.id] = cur;
+        return accu;
+      }, {})
+    }
+  };
+
+  normalized.result.length
+    ? res.status(200).json(normalized)
+    : res
+        .status(404)
+        .json({ message: 'There are no questionTypes for that question.' });
+});
+
 module.exports = router;
