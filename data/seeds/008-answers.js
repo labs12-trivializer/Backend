a6fa598117dcdfa28f723ac5c8d6d368039f0ae8
heyjuststart <41658450+heyjuststart@users.noreply.github.com>
2019-05-22 20:59:40 -0400

    seed only 4 answers max per question

--- data/seeds/008-answers.js
+++ data/seeds/008-answers.js
@@ -7,13 +7,26 @@ faker.seed(123);
 exports.seed = async function(knex) {
   const dbQuestions = await knex('questions');
 
+  const answerCounts = {};
   const answers = [];
-  for(let i = 0; i < 1000; i++) {
+  for (let i = 0; i < 1000; i++) {
+    let thisQuestion = randomItem(dbQuestions).id;
+    while (
+      answerCounts[thisQuestion] &&
+      answerCounts[thisQuestion] > 3
+    ) {
+      thisQuestion = randomItem(dbQuestions).id;
+    }
+
     answers.push({
       text: faker.hacker.phrase(),
-      question_id: randomItem(dbQuestions).id,
+      question_id: thisQuestion,
       is_correct: randomItem([true, false])
     });
+    answerCounts[thisQuestion] =
+      typeof answerCounts[thisQuestion] !== 'undefined'
+        ? answerCounts[thisQuestion] + 1
+        : 1;
   }
 
   await knex.batchInsert('answers', answers, 30);
