ddae05c6ed71f5cede5a366e7345216ed1c9ee1f
Jonathan Picazo <macbethjonathan@gmail.com>
2019-05-23 16:32:01 -0400

    add auth0 module and update users route

--- middleware/restricted.js
+++ middleware/restricted.js
@@ -6,10 +6,10 @@ const jwtCheck = jwt({
     cache: true,
     rateLimit: true,
     jwksRequestsPerMinute: 5,
-    jwksUri: 'https://dev-d9y68pfa.auth0.com/.well-known/jwks.json'
+    jwksUri: `https://${process.env.AUTH0_DOMAIN}/.well-known/jwks.json`
   }),
-  audience: 'https://lambda-trivializer.herokuapp.com/api',
-  issuer: 'https://dev-d9y68pfa.auth0.com/',
+  audience: process.env.AUTH0_AUDIENCE,
+  issuer: `https://${process.env.AUTH0_DOMAIN}/`,
   algorithms: ['RS256']
 });
 
